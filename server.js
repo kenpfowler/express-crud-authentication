f908575876cf19de7a691c4b3fb0a9ee92061fb2
Kenny <kenpfowler@gmail.com>
2021-05-20 15:13:31 -0400

    added routes for /info and /contact paths

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello wide World!");
 });
 
+app.get("/info", (req, res) => {
+  res.send("site info");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Please contact us!");
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
