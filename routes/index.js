d606e43084314830521bc56fba46248a91e40a81
Kenny <kenpfowler@gmail.com>
2021-05-20 17:07:29 -0400

    added routes and unique title properties for various url paths

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,49 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
-    title: "Express",
-    user: "Ken",
+    title: "Home",
+  });
+});
+
+/* GET home page if user types home */
+router.get("/home", function (req, res, next) {
+  res.render("index", {
+    title: "Home",
+  });
+});
+
+/* GET About page. */
+router.get("/about", function (req, res, next) {
+  res.render("index", {
+    title: "About",
+  });
+});
+
+/* GET Poducts page. */
+router.get("/products", function (req, res, next) {
+  res.render("index", {
+    title: "Products",
+  });
+});
+
+/* GET Services page. */
+router.get("/services", function (req, res, next) {
+  res.render("index", {
+    title: "Services",
+  });
+});
+
+/* GET About Us page. */
+router.get("/aboutus", function (req, res, next) {
+  res.render("index", {
+    title: "About Us",
+  });
+});
+
+/* GET Contact Us page. */
+router.get("/contact", function (req, res, next) {
+  res.render("index", {
+    title: "Contact",
   });
 });
 
